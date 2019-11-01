88e80918db2877b064973b30f663b8ad301b606d
Andre Aditya P <andre02.9d@gmail.com>
2019-11-01 09:55:11 +0700

    add login module, add auth module

--- models/user.js
+++ models/user.js
@@ -1,4 +1,7 @@
 const mongoose = require('mongoose')
+const validator = require('validator')
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
 const userSchema = new mongoose.Schema({
   name :{
     type: String,
@@ -6,12 +9,63 @@ const userSchema = new mongoose.Schema({
   },
   email: {
     type: String,
-    required: true
+    unique: true,
+    required: true,
+    validate: value => {
+      if(!validator.isEmail(value)) {
+        throw new Error({error: 'invalid email address'})
+      }
+    }
   },
   created_at:{
     type: Date,
     required: true,
     default: Date.now
+  },
+  password:{
+    type: String,
+    required: true,
+    minLength: 7
+  },
+  tokens:[{
+    token: {
+      type: String,
+      required: true
+    }
   }
+
+  ]
+
 })
-module.exports = mongoose.model('User', userSchema)
+
+userSchema.pre('save', async function(next){
+  const user = this
+  if(user.isModified('password')){
+    user.password = await bcrypt.hash(user.password, 8)
+  }
+  next()
+})
+
+userSchema.methods.generateAuthToken = async function(){
+  const user = this
+  const token = jwt.sign({_id: user._id}, process.env.JWT_KEY)
+  user.tokens = user.tokens.concat({token})
+  await user.save()
+  return token
+}
+
+userSchema.statics.findByCredentials = async (email, password)=>{
+  const user = await User.findOne({email})
+  if(!user) {
+    throw new Error({error: 'invalid login'})
+  }
+  const isPasswordMatch = await bcrypt.compare(password, user.password)
+  if(!isPasswordMatch) {
+    throw new Error({error: 'invalid login'})
+  }
+  return user
+}
+const User = mongoose.model('User', userSchema)
+
+module.exports = User
+// module.exports = mongoose.model('User', userSchema)
--- routes/users.js
+++ routes/users.js
@@ -4,6 +4,8 @@ const express = require('express')
 const router = express.Router()
 const User = require('../models/user')
 const helper = require('./helper')
+const auth = require('./auth')
+
 
 // list all users
 router.get('/', async (req, res) => {
@@ -22,19 +24,21 @@ router.get('/:id', helper, (req, res) => {
 
 // create a new user
 router.post('/', async (req, res) => {
-    const user = new User({
-        name: req.body.name,
-        email: req.body.email
-    })
+    const user = new User(req.body)
 
     try {
-        const newUser = await user.save()
-        res.status(201).json(newUser)
+        await user.save()
+        const token = await user.generateAuthToken()
+        res.status(201).send({user, token})
     } catch (err) {
         res.status(400).json({message: err.message})
     }
 })
-
+// router.post('/login',async (req,res)=>{
+//   try{
+//     const {}
+//   }
+// })
 // update a user
 router.patch('/:id', helper, async (req, res) => {
     if (req.body.name != null) {
@@ -55,10 +59,41 @@ router.patch('/:id', helper, async (req, res) => {
 router.delete('/:id', helper, async (req, res) => {
     try {
         await res.user.remove()
-        res.json({message: "user is deleted"})
+        res.json({message: "berhasil dihapus"})
     } catch(err) {
         res.status(500).json({message: err.message})
     }
 })
 
+router.post('/login',async (req,res)=>{
+  try{
+    const {email, password} = req.body
+    const user = await User.findByCredentials(email, password)
+    if(!user){
+      return res.status(401).send({message: 'gagal'})
+    }
+
+    const token = await user.generateAuthToken()
+    res.send({user,token})
+  }catch(err){
+    res.status(400).send({message: 'bad request'})
+  }
+
+  router.get('/me',auth,async(req,res)=>{
+    res.send(req.user)
+  })
+
+  router.post('/me/logout',auth,async(req,res)=>{
+    try{
+      req.user.tokens = req.user.tokens.filter((token)=>{
+        return token.token != req.token
+      })
+      await req.user.save()
+      res.send()
+    }catch (err){
+      res.status(500).send(err)
+    }
+  })
+
+})
 module.exports = router
